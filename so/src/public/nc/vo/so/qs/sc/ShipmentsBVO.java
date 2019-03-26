fcc3d93e1eb04fc307bf4f12aae3ae8f7d327a6a
ccaa1032 <281771654@qq.com>
2019-03-26 17:49:04 +0800

    190326

--- so/src/public/nc/vo/so/qs/sc/ShipmentsBVO.java
+++ so/src/public/nc/vo/so/qs/sc/ShipmentsBVO.java
@@ -176,83 +176,83 @@ public UFDate darrdate;
 /**
 *自定义项1
 */
-public String def1;
+public String vbdef1;
 /**
 *自定义项10
 */
-public String def10;
+public String vbdef10;
 /**
 *自定义项11
 */
-public String def11;
+public String vbdef11;
 /**
 *自定义项12
 */
-public String def12;
+public String vbdef12;
 /**
 *自定义项13
 */
-public String def13;
+public String vbdef13;
 /**
 *自定义项14
 */
-public String def14;
+public String vbdef14;
 /**
 *自定义项15
 */
-public String def15;
+public String vbdef15;
 /**
 *自定义项16
 */
-public String def16;
+public String vbdef16;
 /**
 *自定义项17
 */
-public String def17;
+public String vbdef17;
 /**
 *自定义项18
 */
-public String def18;
+public String vbdef18;
 /**
 *自定义项19
 */
-public String def19;
+public String vbdef19;
 /**
 *自定义项2
 */
-public String def2;
+public String vbdef2;
 /**
 *自定义项20
 */
-public String def20;
+public String vbdef20;
 /**
 *自定义项3
 */
-public String def3;
+public String vbdef3;
 /**
 *自定义项4
 */
-public String def4;
+public String vbdef4;
 /**
 *自定义项5
 */
-public String def5;
+public String vbdef5;
 /**
 *自定义项6
 */
-public String def6;
+public String vbdef6;
 /**
 *自定义项7
 */
-public String def7;
+public String vbdef7;
 /**
 *自定义项8
 */
-public String def8;
+public String vbdef8;
 /**
 *自定义项9
 */
-public String def9;
+public String vbdef9;
 /**
 *要求到货日期
 */
@@ -284,7 +284,7 @@ public String marbasclassid;
 /**
 *备注
 */
-public String memo;
+public String vmemo;
 /**
 *混合料价
 */
@@ -1328,360 +1328,7 @@ this.darrdate=darrdate;
 *
 * @return 自定义项1
 */
-public String getDef1 () {
-return this.def1;
- } 
 
-/** 
-* 设置自定义项1
-*
-* @param def1 自定义项1
-*/
-public void setDef1 ( String def1) {
-this.def1=def1;
- } 
-
-/** 
-* 获取自定义项10
-*
-* @return 自定义项10
-*/
-public String getDef10 () {
-return this.def10;
- } 
-
-/** 
-* 设置自定义项10
-*
-* @param def10 自定义项10
-*/
-public void setDef10 ( String def10) {
-this.def10=def10;
- } 
-
-/** 
-* 获取自定义项11
-*
-* @return 自定义项11
-*/
-public String getDef11 () {
-return this.def11;
- } 
-
-/** 
-* 设置自定义项11
-*
-* @param def11 自定义项11
-*/
-public void setDef11 ( String def11) {
-this.def11=def11;
- } 
-
-/** 
-* 获取自定义项12
-*
-* @return 自定义项12
-*/
-public String getDef12 () {
-return this.def12;
- } 
-
-/** 
-* 设置自定义项12
-*
-* @param def12 自定义项12
-*/
-public void setDef12 ( String def12) {
-this.def12=def12;
- } 
-
-/** 
-* 获取自定义项13
-*
-* @return 自定义项13
-*/
-public String getDef13 () {
-return this.def13;
- } 
-
-/** 
-* 设置自定义项13
-*
-* @param def13 自定义项13
-*/
-public void setDef13 ( String def13) {
-this.def13=def13;
- } 
-
-/** 
-* 获取自定义项14
-*
-* @return 自定义项14
-*/
-public String getDef14 () {
-return this.def14;
- } 
-
-/** 
-* 设置自定义项14
-*
-* @param def14 自定义项14
-*/
-public void setDef14 ( String def14) {
-this.def14=def14;
- } 
-
-/** 
-* 获取自定义项15
-*
-* @return 自定义项15
-*/
-public String getDef15 () {
-return this.def15;
- } 
-
-/** 
-* 设置自定义项15
-*
-* @param def15 自定义项15
-*/
-public void setDef15 ( String def15) {
-this.def15=def15;
- } 
-
-/** 
-* 获取自定义项16
-*
-* @return 自定义项16
-*/
-public String getDef16 () {
-return this.def16;
- } 
-
-/** 
-* 设置自定义项16
-*
-* @param def16 自定义项16
-*/
-public void setDef16 ( String def16) {
-this.def16=def16;
- } 
-
-/** 
-* 获取自定义项17
-*
-* @return 自定义项17
-*/
-public String getDef17 () {
-return this.def17;
- } 
-
-/** 
-* 设置自定义项17
-*
-* @param def17 自定义项17
-*/
-public void setDef17 ( String def17) {
-this.def17=def17;
- } 
-
-/** 
-* 获取自定义项18
-*
-* @return 自定义项18
-*/
-public String getDef18 () {
-return this.def18;
- } 
-
-/** 
-* 设置自定义项18
-*
-* @param def18 自定义项18
-*/
-public void setDef18 ( String def18) {
-this.def18=def18;
- } 
-
-/** 
-* 获取自定义项19
-*
-* @return 自定义项19
-*/
-public String getDef19 () {
-return this.def19;
- } 
-
-/** 
-* 设置自定义项19
-*
-* @param def19 自定义项19
-*/
-public void setDef19 ( String def19) {
-this.def19=def19;
- } 
-
-/** 
-* 获取自定义项2
-*
-* @return 自定义项2
-*/
-public String getDef2 () {
-return this.def2;
- } 
-
-/** 
-* 设置自定义项2
-*
-* @param def2 自定义项2
-*/
-public void setDef2 ( String def2) {
-this.def2=def2;
- } 
-
-/** 
-* 获取自定义项20
-*
-* @return 自定义项20
-*/
-public String getDef20 () {
-return this.def20;
- } 
-
-/** 
-* 设置自定义项20
-*
-* @param def20 自定义项20
-*/
-public void setDef20 ( String def20) {
-this.def20=def20;
- } 
-
-/** 
-* 获取自定义项3
-*
-* @return 自定义项3
-*/
-public String getDef3 () {
-return this.def3;
- } 
-
-/** 
-* 设置自定义项3
-*
-* @param def3 自定义项3
-*/
-public void setDef3 ( String def3) {
-this.def3=def3;
- } 
-
-/** 
-* 获取自定义项4
-*
-* @return 自定义项4
-*/
-public String getDef4 () {
-return this.def4;
- } 
-
-/** 
-* 设置自定义项4
-*
-* @param def4 自定义项4
-*/
-public void setDef4 ( String def4) {
-this.def4=def4;
- } 
-
-/** 
-* 获取自定义项5
-*
-* @return 自定义项5
-*/
-public String getDef5 () {
-return this.def5;
- } 
-
-/** 
-* 设置自定义项5
-*
-* @param def5 自定义项5
-*/
-public void setDef5 ( String def5) {
-this.def5=def5;
- } 
-
-/** 
-* 获取自定义项6
-*
-* @return 自定义项6
-*/
-public String getDef6 () {
-return this.def6;
- } 
-
-/** 
-* 设置自定义项6
-*
-* @param def6 自定义项6
-*/
-public void setDef6 ( String def6) {
-this.def6=def6;
- } 
-
-/** 
-* 获取自定义项7
-*
-* @return 自定义项7
-*/
-public String getDef7 () {
-return this.def7;
- } 
-
-/** 
-* 设置自定义项7
-*
-* @param def7 自定义项7
-*/
-public void setDef7 ( String def7) {
-this.def7=def7;
- } 
-
-/** 
-* 获取自定义项8
-*
-* @return 自定义项8
-*/
-public String getDef8 () {
-return this.def8;
- } 
-
-/** 
-* 设置自定义项8
-*
-* @param def8 自定义项8
-*/
-public void setDef8 ( String def8) {
-this.def8=def8;
- } 
-
-/** 
-* 获取自定义项9
-*
-* @return 自定义项9
-*/
-public String getDef9 () {
-return this.def9;
- } 
-
-/** 
-* 设置自定义项9
-*
-* @param def9 自定义项9
-*/
-public void setDef9 ( String def9) {
-this.def9=def9;
- } 
 
 /** 
 * 获取要求到货日期
@@ -1818,18 +1465,7 @@ this.marbasclassid=marbasclassid;
 *
 * @return 备注
 */
-public String getMemo () {
-return this.memo;
- } 
 
-/** 
-* 设置备注
-*
-* @param memo 备注
-*/
-public void setMemo ( String memo) {
-this.memo=memo;
- } 
 
 /** 
 * 获取混合料价
@@ -3182,7 +2818,177 @@ this.vsrctype=vsrctype;
  } 
 
 
-  @Override
+
+
+  public String getVbdef1() {
+	return vbdef1;
+}
+
+public void setVbdef1(String vbdef1) {
+	this.vbdef1 = vbdef1;
+}
+
+public String getVbdef10() {
+	return vbdef10;
+}
+
+public void setVbdef10(String vbdef10) {
+	this.vbdef10 = vbdef10;
+}
+
+public String getVbdef11() {
+	return vbdef11;
+}
+
+public void setVbdef11(String vbdef11) {
+	this.vbdef11 = vbdef11;
+}
+
+public String getVbdef12() {
+	return vbdef12;
+}
+
+public void setVbdef12(String vbdef12) {
+	this.vbdef12 = vbdef12;
+}
+
+public String getVbdef13() {
+	return vbdef13;
+}
+
+public void setVbdef13(String vbdef13) {
+	this.vbdef13 = vbdef13;
+}
+
+public String getVbdef14() {
+	return vbdef14;
+}
+
+public void setVbdef14(String vbdef14) {
+	this.vbdef14 = vbdef14;
+}
+
+public String getVbdef15() {
+	return vbdef15;
+}
+
+public void setVbdef15(String vbdef15) {
+	this.vbdef15 = vbdef15;
+}
+
+public String getVbdef16() {
+	return vbdef16;
+}
+
+public void setVbdef16(String vbdef16) {
+	this.vbdef16 = vbdef16;
+}
+
+public String getVbdef17() {
+	return vbdef17;
+}
+
+public void setVbdef17(String vbdef17) {
+	this.vbdef17 = vbdef17;
+}
+
+public String getVbdef18() {
+	return vbdef18;
+}
+
+public void setVbdef18(String vbdef18) {
+	this.vbdef18 = vbdef18;
+}
+
+public String getVbdef19() {
+	return vbdef19;
+}
+
+public void setVbdef19(String vbdef19) {
+	this.vbdef19 = vbdef19;
+}
+
+public String getVbdef2() {
+	return vbdef2;
+}
+
+public void setVbdef2(String vbdef2) {
+	this.vbdef2 = vbdef2;
+}
+
+public String getVbdef20() {
+	return vbdef20;
+}
+
+public void setVbdef20(String vbdef20) {
+	this.vbdef20 = vbdef20;
+}
+
+public String getVbdef3() {
+	return vbdef3;
+}
+
+public void setVbdef3(String vbdef3) {
+	this.vbdef3 = vbdef3;
+}
+
+public String getVbdef4() {
+	return vbdef4;
+}
+
+public void setVbdef4(String vbdef4) {
+	this.vbdef4 = vbdef4;
+}
+
+public String getVbdef5() {
+	return vbdef5;
+}
+
+public void setVbdef5(String vbdef5) {
+	this.vbdef5 = vbdef5;
+}
+
+public String getVbdef6() {
+	return vbdef6;
+}
+
+public void setVbdef6(String vbdef6) {
+	this.vbdef6 = vbdef6;
+}
+
+public String getVbdef7() {
+	return vbdef7;
+}
+
+public void setVbdef7(String vbdef7) {
+	this.vbdef7 = vbdef7;
+}
+
+public String getVbdef8() {
+	return vbdef8;
+}
+
+public void setVbdef8(String vbdef8) {
+	this.vbdef8 = vbdef8;
+}
+
+public String getVbdef9() {
+	return vbdef9;
+}
+
+public void setVbdef9(String vbdef9) {
+	this.vbdef9 = vbdef9;
+}
+
+public String getVmemo() {
+	return vmemo;
+}
+
+public void setVmemo(String vmemo) {
+	this.vmemo = vmemo;
+}
+
+@Override
   public IVOMeta getMetaData() {
     return VOMetaFactory.getInstance().getVOMeta("so.ShipmentsBVO");
   }
